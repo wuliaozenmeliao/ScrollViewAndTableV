117f8eae5aa74c22c80aacdfb8fcd9b95cc29010
xinguo <xinguo_app@163.com>
2017-11-15 10:27:05 +0800

    Initial Commit

---
+++ ScrollAndTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollAndTable
+//
+//  Created by XinGou on 2017/11/15.
+//  Copyright © 2017年 XinGou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollAndTable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollAndTable
+//
+//  Created by XinGou on 2017/11/15.
+//  Copyright © 2017年 XinGou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
